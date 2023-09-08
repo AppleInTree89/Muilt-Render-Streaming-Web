a19d60be525482bc3619cc4d89ffcebe7226ddfb
SMR\87023 <870236070@qq.com>
2023-09-08 09:21:11 +0800

    画面同步完成

--- src/components/RS.vue
+++ src/components/RS.vue
@@ -1,79 +1,110 @@
 <script setup lang="ts">
-import { onMounted,onUnmounted } from "vue";
+import { onMounted,onUnmounted, ref } from "vue";
 import{io,Socket} from "socket.io-client"
 let socket:Socket
 let rtc:RTCPeerConnection
-function AskForConnect()
-{
-  let op:RTCOfferOptions={};
-  op.iceRestart=false;
-  op.offerToReceiveAudio=true;
-  op.offerToReceiveVideo=true
-  //rtc.createDataChannel("11")
- // .protocol.rtc.addTrack
-  rtc.createOffer(op).then(res=>{
-    rtc.setLocalDescription(res)
-    socket.emit("AskConnect",res.sdp)
-  })
-  rtc.onicecandidate= (event) => {
-			var candidate = event.candidate;
-      //console.log(candidate)
-      if(candidate==null)
-      {
-        return
-      }
-      socket.emit("ice",{
-        candidate: candidate.candidate,
-        sdpMid: candidate.sdpMid,
-        sdpMLineIndex: candidate.sdpMLineIndex,
-      })
-	}
-  rtc.onconnectionstatechange= () => {
-    console.log(rtc.connectionState.toString())
-  }
-
-  rtc.ontrack=(event)=>
-  {
-    if(event.track.kind=="video")
-			{
-				console.log("vvvvvvvvvvvvvvvvvvvvvvvvvvv")
-				const _remoteStream:MediaStream=new MediaStream();
-				_remoteStream.addTrack(event.track);
-				  var video:HTMLVideoElement = document.getElementById("video") as HTMLVideoElement ;
-         video.autoplay=true;
-          video .srcObject =_remoteStream;
-			}
-		
-  }
-
+let _remoteStream:MediaStream
+let _dataChannel:RTCDataChannel
+const vbtnShow=ref<boolean>(true)
+const vplayer = ref<HTMLVideoElement>()
+const vbtn=ref<HTMLButtonElement>()
+const servers = {
+			iceServers: [{
+				urls: ["turn:asset.yz-rxrj.com?transport=udp"],
+				credential: "admin",
+				username :"123456",
+				credentialType :"Password",
+  }],
 }
+function test()
+{
+  initRTC()
+  vbtnShow.value=false
+  document.oncontextmenu=function()//禁用鼠标右键
+	{
+		return false;
+	}
+  socket.emit("init",(response:any)=>
+  {
+    console.log(response)
+    //vbtn.value?.style.visibility="hidden"
+  })
+  socket.on("offer",(data)=>
+  {
+    const desc = new RTCSessionDescription({
+						sdp: data,
+						type: "offer"});
+    rtc.setRemoteDescription(desc).then(() => {
+      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+            createAns()
+					})
+  })
+  socket.on("ice",(data)=>
+  {
+    rtc.addIceCandidate(data);
+  })
+}
+function initRTC()
+{
+    rtc = new RTCPeerConnection(servers);
+    rtc.ontrack = (event) => {
+			if(event.track.kind=="video")
+			{
+				_remoteStream=new MediaStream();
+				_remoteStream.addTrack(event.track);
+			   (vplayer.value as HTMLVideoElement).autoplay=true;
+         (vplayer.value as HTMLVideoElement).srcObject =_remoteStream;
+			}
+			else
+			{
+				_remoteStream=new MediaStream();
+				_remoteStream.addTrack(event.track);
+				//this._audio.autoplay=true;
+				//this._audio.srcObject =_remoteStream;
+			}
+			
+		}
+		//交换信令
+		rtc.onicecandidate = (event) => {
+			var candidate = event.candidate;
+			if (candidate != null) {
+				var data = {
+					candidate: candidate.candidate,
+					sdpMid: candidate.sdpMid,
+					sdpMLineIndex: candidate.sdpMLineIndex
+				};
+				console.log(data)
+			}
+		}
+		//文字消息交换
+		rtc.ondatachannel = (event) => {
+			_dataChannel = event.channel;
+			_dataChannel.onmessage = (event) => {
+				var data=JSON.parse(event.data);
+        console.log(data)
+			}
+  }
+}
+function createAns()
+{
+  rtc.createAnswer().then((res) => {
+    rtc.setLocalDescription(res)
+    socket.emit("answer",res.sdp)
+		})
+}
+
+
 onMounted(() => {
-    rtc=new RTCPeerConnection()
-    socket=io("ws://127.0.0.1:3000",{auth:{type:"RSWeb"}})
+    socket=io("http://127.0.0.1:3000",{auth:{type:"RSWeb"}})
     socket.on("connect", () => {
     console.log("链接上中央服务器"); // undefined
     socket.on("disconnect", () => 
-    {
-        console.log("与中央服务器链接断开"); // undefined
+    { console.log("与中央服务器链接断开"); // undefined
     });
-    socket.on("answer", (data) => 
-    {
-      const desc = new RTCSessionDescription({
-						sdp:data,
-						type: "answer"
-					});
-          rtc.setRemoteDescription(desc)
+    socket.on("error",(e) => 
+    { 
+      alert(e)
     });
-    socket.on("error", (data) => 
-    {
-       alert(data)// undefined
-    });
-    socket.on("wice",(data)=>
-    {
-      rtc.addIceCandidate(data)
-      console.log(data)
-    })
-
   });
 })
 onUnmounted(() => {
@@ -83,14 +114,51 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <button @click="AskForConnect">link</button>
-  <video id="video"></video>
+
+  <div id="player">
+    <video ref="vplayer" id="tdVideo"></video>
+    <audio id="tdAudio"></audio>
+    
+    <button ref="vbtn" v-show="vbtnShow" @click="test" id="playButton"></button>
+  </div>
 </template>
 
 <style scoped>
-.video
-{
+body{
+  margin: 0px;
+  background-color: white;
+}
+
+#player{
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+}
+
+#tdVideo{
+	z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
+}
+
+#playButton{
+  z-index: initial;
+  width: 15vw;
+  height: 15vw;
+  max-width: 200px;
+  max-height: 200px;
+  background:url("/vite.svg") no-repeat;
+  background-size: contain;
+  background-position:center center;
+  border: 0px;
 }
 </style>
